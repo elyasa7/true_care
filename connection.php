e5371c22946054e68c0106cddef19e97995aff0c
Ilyas Jumadurdyew <ilyas@open.tm>
2018-12-23 05:18:53 +0500

    Update remove some files

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$mysql_database='true_care';
-$mysql_host='127.0.0.1';
-$mysql_user='root';
-$mysql_password='123';
+$mysql_database='your_database';
+$mysql_host='your_host or 127.0.0.1';
+$mysql_user='db_username';
+$mysql_password='db_pass';
 
 @mysql_connect($mysql_host, $mysql_user, $mysql_password) or die('Connection Failed');
 @mysql_select_db($mysql_database) or die('Could not connect to DB');
--- function/make_call.php
+++ function/make_call.php
@@ -6,16 +6,16 @@ require_once '../twilio-php/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'ACa995d7b14b830d8c2760844ffdfa1aa1';
-$auth_token = 'b14e50acb7df2a06aedf7cb2eb6d5471';
+$account_sid = 'your_twilio_account_sid';
+$auth_token = 'your_twilio_auth_token';
 // In production, these should be environment variables. E.g.:
 // $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
 
 // A Twilio number you own with Voice capabilities
-$twilio_number = "+12098507617";
+$twilio_number = "+1234567890";
 
 // Where to make a voice call (your cell phone?)
-$to_number = "+905313435178"; # Admin phone number
+$to_number = "+0987654321"; # Admin phone number
 //$agent_number = $_POST["phone_to_call"]; # Phone to be connected
 $agent_number = str_replace(' ', '', $_POST["phone_to_call"]); # Phone to be connected
 
@@ -26,8 +26,7 @@ $client->account->calls->create(
 	$to_number,
 	$twilio_number,
 	array(
-		//"url" => "http://demo.twilio.com/docs/voice.xml"
-		"url" => "https://open.tm/call.php?phone=$agent_number"
+		"url" => "https://yourdomain.com/call.php?phone=$agent_number"
 	)
 );
 
